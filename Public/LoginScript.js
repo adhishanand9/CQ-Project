bcc949a49d70227aaebaa7f306595d834232b99c
Adhish Anand <adhishanand9@gmail.com>
2019-05-12 12:38:05 +0530

    [Updated DB]

--- Public/LoginScript.js
+++ Public/LoginScript.js
@@ -1,10 +1,28 @@
-var submit=document.getElementById("login_button");
-var user=document.getElementById("username_input");
-var password=document.getElementById("password_input");
-submit.addEventListener('click',function(){
-  if(user.innerHTML=="")
-  {
-    alert("Incomplete Field");
-  }
-  console.log(1);
+var loginBtn=document.getElementById("login_button");
+var userName=document.getElementById("username_input");
+var passWord=document.getElementById("password_input");
+loginBtn.addEventListener('click',function(event){
+    if(userName.value == ""|| passWord.value == ""){
+        alert("All Fields are required");
+    } else {
+        var userdata = new Object();
+        userdata.userName = userName.value;
+        userdata.passWord = passWord.value;
+        console.log(userdata);
+        var request = new XMLHttpRequest();
+        request.open("POST",'/login');
+        request.setRequestHeader("Content-Type","application/json");
+        request.send(JSON.stringify(userdata));
+        request.onload = function(){
+            var dataReturned = JSON.parse(request.responseText);
+            if(dataReturned.length==""){
+                alert('User not exist');
+                userName.value="";
+                passWord.value="";
+            } else {
+                console.log(dataReturned);
+                location.href = "/home"
+            }
+        }
+    }
 });
--- app.js
+++ app.js
@@ -1,33 +1,81 @@
 var express = require('express')
-var path = require('path')
-var app = express()
+var path = require('path');
+var app = express();
+var session = require('express-session');
 
-app.use(express.static(path.join(__dirname,'public')))
+//Access Static files
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+//BodyParser
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(session({secret: 'cQpOrTaL',saveUninitialized:true,resave:true}));
 
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost/cqDB';
+// Connect with db
+var mongoose = require('mongoose')
+var mongoDb = 'mongodb://localhost/myDB'
 
-mongoose.connect(mongoDB);
-mongoose.connection.on('error',(err) => {
-  console.log('DB connection Error');
+mongoose.connect(mongoDb, function (error) {
+	if(error) {
+		throw error;
+	}
+	console.log("Db opened Successfully");
+});
+
+var userSchema = mongoose.Schema({
+	name: String,
+	email: String,
+	password: String,
+	city: String,
+	phoneno: String,
+	gender: String,
+	dob: String,
+	role: String
+});
+
+var userDetails = mongoose.model("userdetails", userSchema);
+
+app.post('/login',function(req,res){
+    userDetails.find({
+        email: req.body.userName,
+        password: req.body.passWord
+    }).exec(function(error,data){
+        console.log(data);
+        req.session.data = data;
+        res.send(data);
+    });
+});
+
+app.get("/home", function(req, res) {
+    res.render('homepage', {data: req.session.data});
 })
 
-mongoose.connection.on('connected',(err) => {
-  console.log('DB connected');
-})
+app.get('/login',function(req,res,next){
+    console.log("helo wordls")
+    if(req.session.isLogin){
+        req.session.isLogin = 0;
+       res.render('homepage', {data: req.session.data});
+    } else {
+        //Ask for id password
+        req.session.isLogin = 1;
+        res.sendFile(path.join(__dirname,'public', 'login.html'));
+    }
+    next();
+});
 
-var userSchema = new mongoose.Schema({
-  Name: String,
-  Email:String,
-  UserPassword:String,
-  Phone:Number,
-  City:String,
-  Role:String,
-  DOB:Date,
-  Gender:String,
-})
+// var middleFunc = function(req,res,next){
+//     if(req.session.isLogin){
+//         console.log("Already LoggedIn");
+//     } else {
+//         //Ask fro id Password
+//         console.log("login set bt middleware");
+//         res.send("Welcome");
+//     }
+// }
 
-var user = mongoose.model('User', userSchema);
+
+app.listen(8000);
+
+
+console.log("Running on port 8000");
